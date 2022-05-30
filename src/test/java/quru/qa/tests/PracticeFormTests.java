5ab637f3a26225c1c4e8a9aea2227c73a47c7d2d
Ekaterina_Vasilevskaya <es-vasilevskaya@yandex.ru>
2022-05-30 12:30:39 +0700

    code without Submit

--- src/test/java/quru/qa/tests/PracticeFormTests.java
+++ src/test/java/quru/qa/tests/PracticeFormTests.java
@@ -41,18 +41,27 @@ public class PracticeFormTests {
 
         //Mobile
         $("#userNumber").setValue("0123456789");
-        sleep(1000);
 
         //Date of Birth
-//        $("#dateOfBirthInput").click();
-//        //$("#dateOfBirthInput").setValue("11 Aug 1992");
-//        //$(".react-datepicker__month-select").selectOption(7);
-//        $(".react-datepicker__month-select").selectOptionContainingText("August");
-//        $(".react-datepicker__year-select").selectOptionContainingText("1992");
-//        $("[aria-label=Choose Tuesday, August 11th, 1992]").click();
+        $("#dateOfBirthInput").click();
+//        //$("#dateOfBirthInput").setValue("11 Aug 1992"); - НЕ работает
+//        //$(".react-datepicker__month-select").selectOption(7); //или selectOptionContainingText("August")
+        $(".react-datepicker__month-select").selectOptionContainingText("August");
+        $(".react-datepicker__year-select").selectOptionContainingText("1992");
+//        $(".react-datepicker__day react-datepicker__day--011").click(); - НЕ находит значение
+        $("[aria-label=\"Choose Tuesday, August 11th, 1992\"]").click();
+//        $("[aria-label=Choose Tuesday, August 11th, 1992]").click(); //- invalid selector: An invalid or illegal selector was specified
+//        $("[label=Choose Tuesday, August 11th, 1992]").click(); - invalid selector: An invalid or illegal selector was specified
+
+
+        //executeJavaScript(String.format("$('#dateOfBirthInput').val('11 August 1992')")); - НЕ отправляет значение
+        //executeJavaScript(String.format("$('%s').datepicker('setDate', '%s')", ".react-datepicker__input-container", "11 August 1992"));- НЕ работает
+        //executeJavaScript(String.format("$('%s').datepicker('setDate', '%s')", "#dateOfBirthInput", "11 August 1992"));- НЕ работает
+        //executeJavaScript(String.format("$('{0}').datepicker('setDate', '{1}')", "#dateOfBirthInput", "11 August 1992"));- НЕ работает
+        sleep(1000);
 
         //Subjects
-//        $("#subjectsInput").setValue("m") - НЕ работает;
+//        $("#subjectsInput").setValue("m"); - НЕ работает
         $("#subjectsInput").sendKeys("Maths");
         $("#subjectsInput").pressEnter();
 
@@ -66,6 +75,20 @@ public class PracticeFormTests {
 
         //Current Address
         $("#currentAddress").setValue("my currentAddress");
+
+        //State
+        //$("#state").click(); - НЕ обязательно, без этого работает
+        $("#react-select-3-input").sendKeys("Haryana");
+        $("#react-select-3-input").pressEnter();
+
+        //City
+        //$("#city").click(); - НЕ обязательно, без этого работает
+        $("#react-select-4-input").sendKeys("Panipat");
+        $("#react-select-4-input").pressEnter();
+
+        sleep(2000);
+
+        $("#submit").click();
 
         sleep(2000);
 
